bdf88d3f9c44045344123fc00119b7552022e787
yihjie <yihjie@gmail.com>
2017-01-15 12:12:34 +0800

    8.1.1.1 Pass username

--- Chapter_08/greeter.py
+++ Chapter_08/greeter.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8
 
-def greet_user():
+def greet_user(username):
     """顯示簡單的問候語"""
-    print("Hello!")
+    print("Hello, " + username.title() +  "!")
 
-greet_user()
+greet_user('jesse')
